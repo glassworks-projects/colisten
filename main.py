812dc7afff20a3027c3f471d3caeb03659da1a7f
Ryan Glassman <44929829+glassworks-projects@users.noreply.github.com>
2022-12-31 15:17:40 -0500

    graph representation

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import pandas as pd
-import numpy as np
-import pickle
 import os
-from scripts.construct_collab_matrix import construct_collab_matrix
+import networkx as nx
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
     data = pd.read_csv(os.path.join(PROJECT_ROOT, 'data', 'edges.csv')).sort_values('artist_a')
-    all_artist_ids = np.unique(np.concatenate((data['artist_a'].unique(), data['artist_b'].unique()), axis=None))
-    idx_dt = {el: idx for idx, el in enumerate(all_artist_ids)}
-    edgelist = data[['artist_a', 'artist_b']].astype('int32')
 
-    data_dir = os.path.join(PROJECT_ROOT, 'data')
-    matrix_file = os.path.join(data_dir, 'collab_matrix.pkl')
+    edgelist = dict()
 
-    if os.path.exists(matrix_file):
-        print('adjacency matrix exists. loading from disk...')
-        with open(matrix_file, 'rb') as file:
-            collab_matrix = pickle.load(file)
-            print('done!')
-    else:
-        print('adjacency matrix not found in data directory. constructing...')
-        collab_matrix = construct_collab_matrix(idx_dt, edgelist, data_dir)
+    def evaluate_row(row):
+        k = frozenset([row['artist_a'], row['artist_b']])
+        if k in edgelist:
+            edgelist[k] += 1
+        else:
+            edgelist[k] = 1
+
+    data.apply(evaluate_row, axis=1)
+    g = nx.Graph()
+
+    for key, value in edgelist.items():
+        a, b = [vtx for vtx in iter(key)]
+        g.add_edge(a, b, weight=value)
+
+    def select_next_artist(artist: str, theta: float):
+        pass
 
 
 if __name__ == "__main__":
