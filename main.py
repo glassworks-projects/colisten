2b51e0d6a43eb881b9676247e5ab907b9f693dce
Ryan Glassman <44929829+glassworks-projects@users.noreply.github.com>
2023-01-30 13:26:50 -0500

    edgelist construction

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pandas as pd
+import numpy as np
 import os
-import networkx as nx
+from igraph import Graph
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+def get_all_artist_ids(df):
+    return sorted(list(set(np.append(df['artist_a'].unique(), df['artist_b'].unique()))))
+
+
+def convert_to_weighted_edgelist(df: pd.DataFrame):
+    # edges must be presorted in ascending order (s.t. artist_a < artist_b)
+    mx = np.sort(df[['artist_a', 'artist_b']].to_numpy(), axis=1)
+    unique, counts = np.unique(mx, return_counts=True, axis=0)
+    return np.c_[unique, counts]
+
+
 def main():
     data = pd.read_csv(os.path.join(PROJECT_ROOT, 'data', 'edges.csv'))
-    g = nx.Graph()
+    edgelist = convert_to_weighted_edgelist(data)
 
-    def construct_graph(row):
-        a = row['artist_a']
-        b = row['artist_b']
-        if (a, b) in g.edges:
-            g.edges[(a, b)]['weight'] += 1
-        else:
-            g.add_edge(a, b, weight=1)
-
-    print('constructing graph...')
-    data.apply(construct_graph, axis=1)
-    print('done!')
+    g = Graph.TupleList(list(map(tuple, edgelist[:, :2].astype('str'))))
 
     def select_next_artist(artist: str, theta: float):
         pass
