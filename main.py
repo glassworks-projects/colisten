07dc8e5f38ec7ffe95d0022aa30c5a7daf521710
Ryan Glassman <44929829+glassworks-projects@users.noreply.github.com>
2023-02-10 11:37:41 -0500

    artist select algorithm full impl

--- main.py
+++ main.py
@@ -18,16 +18,24 @@ def convert_to_weighted_edgelist(df: pd.DataFrame):
 
 
 # TODO: decide if we really need to do this, given that computing for the entire graph is reasonably fast
-def ego_subgraph(g: Graph, vertices, order=1):
-    return g.induced_subgraph(g.neighborhood(vertices, order))
+def ego_subgraph(g: Graph, vertex_id: int, order: int = 1):
+    return g.induced_subgraph(g.neighborhood(vertex_id, order))
 
 
-def path_lengths(g: Graph, focal_vertex: str, order=1):
+def path_lengths(g: Graph, focal_vertex_id: int, order=1):
     # for each vertex in subgraph, find path length from that vertex to focal_vertex
     # return: {vertex : length}
-    subgraph = ego_subgraph(g, focal_vertex, order)
-    shortest_paths = subgraph.get_shortest_paths(focal_vertex, weights='weight')
-    return {subgraph.vs()[path[-1]]['name']: compute_single_path_length(subgraph, path) for path in shortest_paths}
+    subgraph = ego_subgraph(g, focal_vertex_id, order)
+    shortest_paths = subgraph.get_shortest_paths(focal_vertex_id, weights='weight')
+    res = {subgraph.vs()[path[-1]]['name']: compute_single_path_length(subgraph, path) for path in shortest_paths}
+    res.pop(subgraph.vs()[focal_vertex_id]['name'])
+    return res
+
+
+# modified softmax
+def probability(lengths: np.ndarray, theta: float):
+    x = np.exp(theta / lengths)
+    return x / sum(x)
 
 
 def compute_single_path_length(graph, path):
@@ -49,6 +57,14 @@ def scale(weight: int):
     return 1 / weight
 
 
+def select_next_artist(g: Graph, artist: str, theta: float, subgraph_order: int = 2):
+    artist_vertex_id = g.vs['name'].index(artist)
+    lengths_dict = path_lengths(g, artist_vertex_id, order=subgraph_order)
+    lengths = np.array(list(lengths_dict.values()))
+    probs = probability(lengths, theta)
+    return np.random.choice(list(lengths_dict.keys()), p=probs)
+
+
 def main():
     data = pd.read_csv(os.path.join(PROJECT_ROOT, 'data', 'edges.csv'))
     print("converting to weighted edgelist...")
@@ -64,10 +80,9 @@ def main():
     )
     print("done!")
 
-    print(path_lengths(g, '1', order=2))
-
-    def select_next_artist(artist: str, theta: float):
-        pass
+    print("sampling for artist 300 ten times: ...")
+    for i in range(10):
+        print(select_next_artist("300", 50))
 
 
 if __name__ == "__main__":
