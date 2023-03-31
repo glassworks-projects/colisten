d39e6960c3e4bbaf65890f3f82fffa99e8389237
Ryan Glassman <44929829+glassworks-projects@users.noreply.github.com>
2023-03-31 19:41:30 -0400

    prevent tight graph loops

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from igraph import Graph
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PREVENT_LOOPS = True
+last_artist_id = ""
 
 
 def convert_to_weighted_edgelist(df: pd.DataFrame):
@@ -20,16 +22,20 @@ def ego_subgraph(g: Graph, vertex_id: int, order: int = 1):
 def path_lengths(g: Graph, artist_id: str, order=1):
     # for each vertex in subgraph, find path length from that vertex to focal_vertex
     # return: {vertex : length}
+    # TODO: can we do this without getting vertex_id, i.e., using "name"?
     focal_vertex_id = g.vs()['name'].index(artist_id)
     subgraph = ego_subgraph(g, focal_vertex_id, order)
-
     # find new vertex ID in subgraph
     focal_vertex_id = subgraph.vs()['name'].index(artist_id)
     shortest_paths = subgraph.get_shortest_paths(focal_vertex_id, weights='weight')
 
     res = {subgraph.vs()[path[-1]]['name']: compute_single_path_length(subgraph, path) for path in shortest_paths}
-    res.pop(artist_id)
 
+    # artificially inflate path length to last visited vertex to discourage looping
+    if last_artist_id and PREVENT_LOOPS:
+        res[last_artist_id] = max(res.values()) + 1
+
+    res.pop(artist_id)
     return res
 
 
@@ -62,12 +68,13 @@ def scale(weight: int):
 
 # TODO experiment with suitable settings of theta
 def select_next_artist(g: Graph, artist_id: str, theta: float, subgraph_order: int = 2):
-    if subgraph_order > 3:
+    global last_artist_id
+    if subgraph_order > 2:
         print("max subgraph order is 2 (for now). {} is too large.".format(subgraph_order))
         subgraph_order = 2
 
     if theta > 5:  # TODO experiment with this
-        print("max setting for theta is 10 (for now). {} is too large.".format(theta))
+        print("max setting for theta is 5 (for now). {} is too large.".format(theta))
         theta = 5
 
     lengths_dict = path_lengths(g, artist_id, order=subgraph_order)
@@ -77,6 +84,7 @@ def select_next_artist(g: Graph, artist_id: str, theta: float, subgraph_order: i
 
 
 def main():
+    global last_artist_id
     data = pd.read_csv(os.path.join(PROJECT_ROOT, 'data', 'edges.csv'))
     print("converting to weighted edgelist...")
     edgelist = convert_to_weighted_edgelist(data)
@@ -92,9 +100,13 @@ def main():
     print("done!")
 
     artist = "417"
+
     for i in range(10):
         next_artist = select_next_artist(g, artist, 0.9)
         print(next_artist)
+
+        # TODO: this juggling of artist IDs needs formalizing
+        last_artist_id = artist
         artist = next_artist
 
 
